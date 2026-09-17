e4333dca3618c51ac9fe5121c6972ab137ebac96
PINAVIBE <juan.josegn18@gmail.com>
2026-09-17 00:11:36 -0500

    inicio de sesion

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gastos-cache-v1';
+const CACHE_NAME = 'gastos-cache-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-192-maskable.png', './icon-512-maskable.png'];
 
 self.addEventListener('install', (event) => {
